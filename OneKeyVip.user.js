ff9836fbbd009da5336c4dfff4a8c6ddf990b0c8
maxzhang666 <maxzhang666@users.noreply.github.com>
2022-02-24 10:10:16 +0000

    update

--- OneKeyVip.user.js
+++ OneKeyVip.user.js
@@ -1,12 +1,12 @@
-// ==UserScript== 
-// @name         【玩的嗨】VIP工具箱,百度文库解析导出,全网VIP视频免费破解去广告,一站式音乐搜索下载,获取B站封面,下载B站视频等众多功能聚合 长期更新,放心使用 
+// ==UserScript==
+// @name         【玩的嗨】VIP工具箱,百度文库解析导出,全网VIP视频免费破解去广告,一站式音乐搜索下载,获取B站封面,下载B站视频等众多功能聚合 长期更新,放心使用
 // @namespace    https://www.wandhi.com/
 // @version      4.3.3
 // @homepage     https://tools.wandhi.com/scripts
 // @supportURL   https://wiki.wandhi.com/
 // @description  🔥功能介绍🔥：🎉 1、Vip视频解析；🎉 2、一站式音乐搜索解决方案；🎉 3、bilibili视频封面获取；🎉 4、bilibili视频下载；🎉 5、上学吧答案查询(已下线)；🎉 6、商品历史价格展示(一次性告别虚假降价)；🎉 7、优惠券查询；🎉 8、CSDN页面、剪切板清理；🎉 9、页面自动展开(更多网站匹配中,欢迎提交想要支持的网站)
 // @author       MaxZhang
-// @icon         https://www.wandhi.com//favicon.ico 
+// @icon         https://www.wandhi.com//favicon.ico
 // @include      *://m.youku.com/v*
 // @include      *://m.youku.com/a*
 // @include      *://v.youku.com/v_*
