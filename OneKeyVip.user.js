f79fc3df86761398507e671e2ad80427ee84acc8
maxzhang666 <maxzhang666@users.noreply.github.com>
2022-05-18 05:28:06 +0000

    update

--- OneKeyVip.user.js
+++ OneKeyVip.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         【玩的嗨】VIP工具箱,百度文库解析导出,全网VIP视频免费破解去广告,一站式音乐搜索下载,获取B站封面,下载B站视频等众多功能聚合 长期更新,放心使用
 // @namespace    https://www.wandhi.com/
-// @version      4.4.1
+// @version      4.4.2
 // @homepage     https://tools.wandhi.com/scripts
 // @supportURL   https://wiki.wandhi.com/
 // @description  🔥功能介绍🔥：🎉 1、Vip视频解析；🎉 2、一站式音乐搜索解决方案；🎉 3、bilibili视频封面获取；🎉 4、bilibili视频下载(已支持分P下载)；🎉 5、上学吧答案查询(已下线)；🎉 6、商品历史价格展示(一次性告别虚假降价)；🎉 7、优惠券查询；🎉 8、CSDN页面、剪切板清理；🎉 9、页面自动展开(更多网站匹配中,欢迎提交想要支持的网站) 🎉 10、YouTube视频下载
@@ -86,12 +86,12 @@
 // @include      *://weibo.cn/sinaurl*
 // @exclude      *://*.wandhi.com/*
 // @require      https://lib.baomitu.com/jquery/1.12.4/jquery.min.js
-// @require      https://cdn.jsdelivr.net/npm/sweetalert2@11
+// @require      https://lib.baomitu.com/limonte-sweetalert2/11.4.7/sweetalert2.all.min.js
 // @require      https://lib.baomitu.com/echarts/4.6.0/echarts.min.js
 // @require      https://lib.baomitu.com/layer/2.3/layer.js
-// @require      https://cdn.jsdelivr.net/npm/vuex@3.4.0/dist/vuex.min.js
-// @require      https://cdn.jsdelivr.net/npm/qrcode@1.4.4/build/qrcode.min.js
-// @require      https://cdn.jsdelivr.net/npm/file-saver@2.0.5/dist/FileSaver.min.js
+// @require      https://lib.baomitu.com/vuex/3.5.1/vuex.min.js
+// @require      https://lib.baomitu.com/qrcodejs/1.0.0/qrcode.min.js
+// @require      https://lib.baomitu.com/FileSaver.js/2.0.5/FileSaver.min.js
 // @license      MIT
 // @grant        GM_setClipboard
 // @run-at       document-end
