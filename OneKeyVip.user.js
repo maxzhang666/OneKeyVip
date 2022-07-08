5125b44db9f2f4f461f8b8e508b752d3a9d1edec
maxzhang666 <maxzhang666@users.noreply.github.com>
2022-07-08 02:18:56 +0000

    update

--- OneKeyVip.user.js
+++ OneKeyVip.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         【玩的嗨】VIP工具箱,百度文库解析导出,全网VIP视频免费破解去广告,一站式音乐搜索下载,获取B站封面,下载B站视频等众多功能聚合 长期更新,放心使用
 // @namespace    https://www.wandhi.com/
-// @version      4.4.7
+// @version      4.4.8
 // @homepage     https://tools.wandhi.com/scripts
 // @supportURL   https://wiki.wandhi.com/
 // @description  🔥功能介绍🔥：🎉 1、Vip视频解析；🎉 2、一站式音乐搜索解决方案；🎉 3、bilibili视频封面获取；🎉 4、bilibili视频下载(已支持分P下载)；🎉 5、上学吧答案查询(已下线)；🎉 6、商品历史价格展示(一次性告别虚假降价)；🎉 7、优惠券查询；🎉 8、CSDN页面、剪切板清理；🎉 9、页面自动展开(更多网站匹配中,欢迎提交想要支持的网站) 🎉 10、YouTube视频下载
@@ -2326,7 +2326,7 @@
             this.init();
         }, BiliImgService.prototype.init = function() {
             Core.autoLazyload((function() {
-                if ($(".video-data").length && $(".bilibili-player-video-info-people-number").length) {
+                if ($(".video-data").length && ($(".bilibili-player-video-info-people-number").length || $(".bpx-player-video-info-online").length)) {
                     var coin = $(".coin").text().replace("\u4e07", "").trim();
                     if ($.isNumeric(coin) || "\u6295\u5e01" == coin) return !0;
                 }
